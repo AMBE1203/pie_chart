8e06ed8899fa84653419bbcd8e1f270af12d1175
Lệ Ngư Hà <duongthecuongbk227@gmail.com>
2021-03-29 23:33:09 +0700

    Update slice_drawer.dart
    add rounded

--- lib/slice_drawer.dart
+++ lib/slice_drawer.dart
@@ -4,6 +4,6 @@ import 'package:pie_chart/pie_chart_data.dart';
 abstract class SliceDrawer {
 
   void drawSlice({Canvas canvas, Size area, double startAngle, double sweepAngle,
-    Slice slice});
+    Slice slice, bool rounded});
 
 }
